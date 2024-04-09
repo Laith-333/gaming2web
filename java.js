90c9d0f79c98743c4b67673ea7e231c8dfc224bd
Laith-333 <82454469+Laith-333@users.noreply.github.com>
2024-04-09 14:05:45 +0200

    s

---
+++ java.js
@@ -0,0 +1,6 @@
+document.querySelector('.buy-button').addEventListener('click', function() {
+    alert('Thank you for your purchase!');
+    // Here you can add more functionality, like redirecting to a checkout page
+    // window.location.href = 'your-checkout-page-url';
+  });
+  
